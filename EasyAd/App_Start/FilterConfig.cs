f5018a9c4cf0c604f3a6da79eb222f825353dbdc
Peng Gao <txq_417@163.com>
2016-10-13 17:54:01 +0800

    添加项目文件。

---
+++ EasyAd/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EasyAd
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
