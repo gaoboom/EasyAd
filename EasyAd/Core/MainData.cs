764e09e0e95e718ecab7cbf8c362d95918750db8
Peng Gao <txq_417@163.com>
2016-10-17 17:30:05 +0800

    Core.MainData and Core.WeixinData added

---
+++ EasyAd/Core/MainData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyAd.Core
+{
+    /// <summary>
+    /// 主表
+    /// <remarks>
+    /// Created at 2016.10.17
+    /// </remarks>
+    /// </summary>
+    public class MainData
+    {
+        [Key]
+        public int MainDataID { get; set; }
+
+        /// <summary>
+        /// 资源名称
+        /// </summary>
+        [Required(ErrorMessage ="{0}必填")]
+        [Display(Name ="资源名称")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 资源类别
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "资源类别")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 所属行业
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "所属行业")]
+        public string Industry { get; set; }
+
+        /// <summary>
+        /// 可投放地区
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "可投放地区")]
+        public string Region { get; set; }
+
+        /// <summary>
+        /// 最后更新时间
+        /// </summary>
+        [Display(Name = "最后更新时间")]
+        public DateTime UpdateTime { get; set; }
+
+        /// <summary>
+        /// 资源可用状态:可用-1，不可用-0，可直接投放-2
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "资源可用状态")]
+        public int Status { get; set; }
+    }
+}
---
+++ EasyAd/Core/WeixinData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyAd.Core
+{
+    /// <summary>
+    /// 微信信息表
+    /// <remarks>
+    /// Created at 2016.10.17
+    /// </remarks>
+    /// </summary>
+    public class WeixinData
+    {
+        [Key]
+        public int WeixinDataID { get; set; }
+
+        /// <summary>
+        /// 微信账号
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "微信账号")]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 账号类别
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "账号类别")]
+        public string AccountType { get; set; }
+
+        /// <summary>
+        /// 粉丝数
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "粉丝数")]
+        public int FunsNum { get; set; }
+
+        /// <summary>
+        /// 推广模式
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "推广模式")]
+        public string AdMode { get; set; }
+
+        /// <summary>
+        /// 推广价格
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "推广价格")]
+        public string AdPrice { get; set; }
+
+        /// <summary>
+        /// 对应主表数据ID
+        /// </summary>
+        public virtual MainData MainData { get; set; }
+    }
+}
