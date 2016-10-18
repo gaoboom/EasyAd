8bc1df63fce526e69b0b388f2f9177139f4dc6ad
Peng Gao <txq_417@163.com>
2016-10-18 12:21:28 +0800

    Core Models finished , database created

---
+++ EasyAd/Core/EasyAdContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EasyAd.Core
+{
+    /// <summary>
+    /// Database Context Class
+    /// <remarsk>
+    /// Created at 2016.10.18
+    /// </remarsk>
+    /// </summary>
+    public class EasyAdContext : DbContext
+    {
+        public DbSet<MainData> MainDataTable { get; set; }
+
+        public DbSet<WebSiteData> WebSiteDataTable { get; set; }
+
+        public DbSet<WeiboData> WeiboDataTable { get; set; }
+
+        public DbSet<WeixinData> WeixinDataTable { get; set; }
+
+        public EasyAdContext():base("EasyAdContext")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<EasyAdContext>());
+        }
+    }
+}
---
+++ EasyAd/Core/WebSiteData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyAd.Core
+{
+    /// <summary>
+    /// WebSite data model
+    /// <remarks>
+    /// Created at 2016.10.18
+    /// </remarks>
+    /// </summary>
+    public class WebSiteData
+    {
+        [Key]
+        public int WebSiteDataID { get; set; }
+
+        /// <summary>
+        /// 网站类别
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "网站类别")]
+        public string SiteType { get; set; }
+
+        /// <summary>
+        /// 网站月均UV
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "月均独立访客量")]
+        public float UVperMonth { get; set; }
+
+        /// <summary>
+        /// 网站月均PV
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "月均页面浏览量")]
+        public float PVperMonth { get; set; }
+
+        /// <summary>
+        /// 网站人均PV
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "人均页面浏览量")]
+        public float PVperUser { get; set; }
+
+        /// <summary>
+        /// 推广模式
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "推广模式")]
+        public string AdMode { get; set; }
+
+        /// <summary>
+        /// 推广价格
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "推广价格")]
+        public string AdPrice { get; set; }
+
+        /// <summary>
+        /// 对应主表数据ID
+        /// </summary>
+        public virtual MainData MainData { get; set; }
+
+    }
+}
